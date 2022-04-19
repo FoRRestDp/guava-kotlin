aa367435bcbd6a90506d2008c62b8f5cb1cd533d
Egor Ponomarev <egorponomarev93@gmail.com>
2022-04-19 19:39:13 +0300

    Removed toStringHelper extension

--- lib/src/main/kotlin/com/github/forrestdp/guava/kotlin/base/MoreObjects.kt
+++
@@ -1,8 +0,0 @@
-package com.github.forrestdp.guava.kotlin.base
-
-import com.google.common.base.MoreObjects
-
-public fun Any.buildToString(builderAction: MoreObjects.ToStringHelper.() -> Unit): String =
-    MoreObjects.toStringHelper(this)
-        .apply(builderAction)
-        .toString()
